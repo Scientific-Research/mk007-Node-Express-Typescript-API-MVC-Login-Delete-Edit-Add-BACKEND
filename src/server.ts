6c1e0e74c7282927e5605e852cc8431f1a6262a6
Maximilian <scientificresearch309@gmail.com>
2025-10-22 01:05:00 +0200

    delete route corrected

--- src/server.ts
+++ src/server.ts
@@ -34,6 +34,7 @@ app.use(express.json());
 
 // const port = 8000;
 const port = process.env.PORT;
+const loginPin = process.env.LoginPIN;
 
 // const jobs = fs.readFileSync('./src/data/jobs.json', 'utf-8'); // in JSON Format => we can not use it => we have to use JSON.parse and then use it like following:
 
@@ -77,7 +78,7 @@ app.get('/todos', (req: express.Request, res: express.Response) => {
 });
 
 app.get('/totaledSkills', (req: express.Request, res: express.Response) => {
-  res.json(getTotaledSkills());
+  res.send(getTotaledSkills());
 });
 
 // NOTE Shows all the Jobs and SkillInfos together extracted from our Lowdb databank => db.json => only for test purposes => we can delete it.
@@ -88,20 +89,30 @@ app.get('/test', (req: express.Request, res: express.Response) => {
 
 app.delete('/jobs/:id', async (req: express.Request, res: express.Response) => {
   const id = Number(req.params.id);
-
+  const pin = req.body.pin;
   // store the deleted Object came back from model.ts(deleteJob function) in deletedObject variable!
-  const deletedObject = await deleteJob(id);
-
-  // when there is not such an object(because it was already deleted or there is no such id) => we get undefined!
-  if (deletedObject === undefined) {
-    res.status(409).send({
+  if (pin !== loginPin) {
+    res.status(401).send({
       error: true,
-      message: `job with id ${id} does not exist, `,
+      statusIdCode: 'badPin',
+      message: `Bad pin.`,
     });
   } else {
-    // when deleted Object is not undefined and is available, we will display it at the output with success code => 200
-    res.status(200).json(deletedObject);
+    const deletedObject = await deleteJob(id);
+
+    // when there is not such an object(because it was already deleted or there is no such id) => we get undefined!
+    if (deletedObject === undefined) {
+      res.status(409).send({
+        error: true,
+        statusIdCode: 'recordDoesNotExist',
+        message: `job with id ${id} does not exist, deletion failed`,
+      });
+    } else {
+      // when deleted Object is not undefined and is available, we will display it at the output with success code => 200
+      res.status(200).json(deletedObject);
+    }
   }
+
   // const nextId = id + 1;
   // res.send(`delete this job with id: ${id} and next id would be: ${nextId}`);
 });
@@ -127,7 +138,7 @@ app.patch('/job', async (req: express.Request, res: express.Response) => {
 app.post('/job', async (req: express.Request, res: express.Response) => {
   const addedJob: IEditedJob = req.body.job;
   const pin: string = req.body.pin;
-  if (pin !== process.env.BACKEND_PIN) {
+  if (pin !== loginPin) {
     res.status(401).send({
       error: true,
       statusIdCode: 'badPin',
@@ -148,7 +159,7 @@ app.post(
   (req: express.Request, res: express.Response) => {
     const pin = req.body.pin;
     console.log(pin);
-    if (pin !== process.env.PIN) {
+    if (pin !== loginPin) {
       res.status(401).send({
         error: true,
         statusIdCode: 'badPin',
